d52d52f20266ccd8004075b3dbde4dca8ef32dc2
Divenesh Shamugam <77532370+Divenesh@users.noreply.github.com>
2026-02-26 05:56:32 +0800

    feat: completed xylophone app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var myPlayer: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func OnButtonClick(_ sender: UIButton) {
+        playSound(soundName: sender.titleLabel?.text! ?? "No Title")
+    }
+    
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName , withExtension: "wav")!
+        
+        do {
+            myPlayer = try AVAudioPlayer(contentsOf: url)
+            myPlayer?.play()
+        } catch {
+            print("Couldn't load the sound file.")
+        }
+    }
+    
 }
 
