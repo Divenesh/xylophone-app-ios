38a7cbca1dca7cd46c05a0fd7cb523e01dcebe61
Divenesh Shamugam <77532370+Divenesh@users.noreply.github.com>
2026-02-26 04:22:46 +0800

    Initial Commit

---
+++ XylophoneTests/XylophoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  XylophoneTests.swift
+//  XylophoneTests
+//
+//  Created by Divenesh Shamugam on 26/02/2026.
+//
+
+import Testing
+@testable import Xylophone
+
+struct XylophoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
